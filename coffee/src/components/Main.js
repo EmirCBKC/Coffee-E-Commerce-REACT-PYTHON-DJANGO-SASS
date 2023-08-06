8e616eecba81de7266030887088cbc772758948b
Emircan ÇUBUKCU <emircancbkc@gmail.com>
2023-08-06 22:16:53 +0300

    install-all-again

---
+++ coffee/src/components/Main.js
@@ -0,0 +1,23 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import React, { useEffect } from 'react';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+import './Main.scss';
+
+function Main() {
+
+    useEffect(() => {
+        AOS.init(); // AOS'u başlatın
+    }, []);
+
+    return (
+        <>
+            <FontAwesomeIcon className='mt-5 ms-5' icon={faCoffee} size="2xl"/>
+            <h1 className='mt-2 ms-5'>Coffee</h1>
+        </>
+    )
+}
+
+export default Main;
