9a38d78254da3d913421f1ee56c10d4fb8e80f0a
Emircan ÇUBUKCU <emircancbkc@gmail.com>
2023-08-13 15:37:49 +0300

    responsive-end

--- coffee/src/components/Main.js
+++ coffee/src/components/Main.js
@@ -20,7 +20,7 @@ function Main() {
         <>
             <MyNavbar />
 
-            <div className='header'>
+            <div className='header mt-4'>
                 <video width="100%" height="100%" autoPlay loop muted>
                     <source src={video}></source>
                 </video>
@@ -29,9 +29,9 @@ function Main() {
             <div className='content mt-5 mb-5 d-flex flex-column justify-content-start align-items-center'>
 
                 <div className='d-flex justify-content-center' data-aos="fade-up" data-aos-anchor-placement="top-bottom">
-                    <div className='info mt-5 mb-5 row justify-content-around align-items-center'>
+                    <div className='info mt-5 mb-5 p-5 row justify-content-around align-items-center'>
                         <h1 className='text-center'>DISCOVER COFFEE</h1>
-                        <div className='info-1 col-xxl-3 col-xl-3 col-lg-3 col-md-12 col-sm-12 col-xs-12 p-5'>
+                        <div className='info-1 mt-5 mb-5 col-xxl-3 col-xl-3 col-lg-8 col-md-10 col-sm-12 col-xs-12 p-5'>
                             <h2 className='text-center'>Coffee Varieties</h2>
                             <p>Explore a diverse range of coffee varieties, from bold and robust to mild and delicate.</p>
                             <p>Discover unique flavor profiles that cater to different preferences, making every sip an adventure.</p>
@@ -39,7 +39,7 @@ function Main() {
                             <p>Whether you enjoy a classic espresso or a flavored blend, our selection has something for everyone.</p>
                             <p>Indulge in the richness of single-origin coffees or the harmonious blends that tell a story in every cup.</p>
                         </div>
-                        <div className='info-2 col-xxl-3 col-xl-3 col-lg-3 col-md-12 col-sm-12 col-xs-12 p-5'>
+                        <div className='info-2 mt-5 mb-5 col-xxl-3 col-xl-3 col-lg-8 col-md-10 col-sm-12 col-xs-12 p-5'>
                             <h2 className='text-center'>Coffee Origins</h2>
                             <p>Dive into the origins of coffee beans, tracing the unique flavors back to different regions worldwide.</p>
                             <p>Explore the distinct taste profiles influenced by altitude, climate, and soil conditions.</p>
@@ -47,7 +47,7 @@ function Main() {
                             <p>Each origin brings a tale of craftsmanship, connecting you to the passionate farmers and their lands.</p>
                             <p>From the highlands of Ethiopia to the plantations of Colombia, every bean carries the essence of its birthplace.</p>
                         </div>
-                        <div className='info-3 col-xxl-3 col-xl-3 col-lg-3 col-md-12 col-sm-12 col-xs-12 p-5'>
+                        <div className='info-3 mt-5 mb-5 col-xxl-3 col-xl-3 col-lg-8 col-md-10 col-sm-12 col-xs-12 p-5'>
                             <h2 className='text-center'>Coffee Presentations</h2>
                             <p>Experience the art of coffee presentation, where every cup is a blend of taste and visual appeal.</p>
                             <p>Admire the intricate latte art that adorns your cappuccino, a testament to barista skills.</p>
@@ -58,13 +58,16 @@ function Main() {
                     </div>
                 </div>
 
-                <div className='first-image mt-5 mb-5 row justify-content-around align-items-center'>
+                <div className='first-image-row mt-3 mb-5 row justify-content-around align-items-center'>
+                    <div className='first-image col-xxl-12 col-xl-12 col-lg-12 col-md-12 col-sm-12 col-xs-12'>
+
+                    </div>
                 </div>
 
                 <div className='d-flex justify-content-center' data-aos="fade-up" data-aos-anchor-placement="top-bottom">
-                    <div className='blog-up mt-5 mb-5 row justify-content-around align-items-center'>
+                    <div className='blog-up mt-5 mb-5 p-5 row justify-content-around align-items-center'>
                         <h1 className='text-center'>COFFEE CUPS</h1>
-                        <div className='blog-up-1 p-5 col-xxl-5 col-xl-5 col-lg-5 col-md-12 col-sm-12 col-xs-12'>
+                        <div className='blog-up-1 mt-5 flex-column mb-5 p-5 col-xxl-5 col-xl-5 col-lg-8 col-md-10 col-sm-12 col-xs-12'>
                             <h2 className='text-center'>Eco-Friendly Choice</h2>
                             <p>Our special coffee cardboard cups are crafted from sustainable materials, reducing environmental impact.</p>
                             <p>Experience guilt-free sips while enjoying your favorite brews in cups designed with the planet in mind.</p>
@@ -77,25 +80,28 @@ function Main() {
                             <p>Share the message of eco-consciousness with each cup, sparking conversations about responsible choices.</p>
                             <p>As you sip from these cups, you're savoring both flavor and a commitment to a healthier planet.</p>
                         </div>
-                        <div className='blog-up-2 d-flex flex-column justify-content-center align-items-center col-xxl-5 col-xl-5 col-lg-5 col-md-12 col-sm-12 col-xs-12'>
+                        <div className='blog-up-2 mt-5 mb-5 d-flex flex-column justify-content-center align-items-center col-xxl-5 col-xl-5 col-lg-8 col-md-10 col-sm-12 col-xs-12'>
                             <div className='cup d-flex flex-column justify-content-center align-items-center'>
-                                <img className='kapak' alt='' src={blogKapak} width="50%"></img>
-                                <img className='bardak' alt='' src={blogCup} width="70%"></img>
+                                <img className='kapak' alt='' src={blogKapak} width="70%"></img>
+                                <img className='bardak' alt='' src={blogCup} width="90%"></img>
                             </div>
                         </div>
                     </div>
                 </div>
 
-                <div className='second-image mt-5 mb-5 row justify-content-around align-items-center'>
+                <div className='second-image-row mt-3 mb-5 row justify-content-around align-items-center'>
+                    <div className='second-image col-xxl-12 col-xl-12 col-lg-12 col-md-12 col-sm-12 col-xs-12'>
+
+                    </div>
                 </div>
 
-                <div className='d-flex justify-content-between' style={{ width: "100%" }} data-aos="fade-up" data-aos-anchor-placement="top-bottom">
-                    <div className='blog-down mt-5 mb-5 row justify-content-between align-items-center'>
+                <div className='d-flex justify-content-center' data-aos="fade-up" data-aos-anchor-placement="top-bottom">
+                    <div className='blog-down mt-5 mb-5 p-5 row justify-content-around align-items-center'>
                         <h1 className='text-center'>COFFEE BEANS</h1>
-                        <div className='blog-down-1 col-5'>
-                            <img alt='' src={blogDownImage} width="90%"></img>
+                        <div className='blog-down-1 d-flex justify-content-center mt-5 mb-5 col-xxl-5 col-xl-5 col-lg-8 col-md-10 col-sm-12 col-xs-12'>
+                            <img alt='' src={blogDownImage} width="80%"></img>
                         </div>
-                        <div className='blog-down-2 me-5 p-5 col-5'>
+                        <div className='blog-down-2 mt-5 mb-5 p-5 col-xxl-5 col-xl-5 col-lg-8 col-md-10 col-sm-12 col-xs-12'>
                             <h2 className='text-center'>From Farm to Cup</h2>
                             <p>Embark on a journey through our coffee beans, from their origins to the final brewed cup.</p>
                             <p>Our carefully selected beans hail from renowned coffee-growing regions across the globe.</p>
@@ -111,25 +117,28 @@ function Main() {
                     </div>
                 </div>
 
-                <div className='three-image mt-5 mb-5 row justify-content-around align-items-center'>
+                <div className='third-image-row mt-3 mb-5 row justify-content-around align-items-center'>
+                    <div className='third-image col-xxl-12 col-xl-12 col-lg-12 col-md-12 col-sm-12 col-xs-12'>
+
+                    </div>
                 </div>
 
                 <div className='d-flex justify-content-center' data-aos="fade-up" data-aos-anchor-placement="top-bottom">
                     <div className='about mt-5 mb-5 row justify-content-around align-items-center'>
                         <h1 className='text-center'>ABOUT US</h1>
-                        <div className='about-1 p-5 col-3'>
+                        <div className='about-1 mt-5 mb-5 p-5 col-xxl-3 col-xl-3 col-lg-8 col-md-10 col-sm-12 col-xs-12'>
                             <h2 className='text-center'>Freshness at Its Best</h2>
                             <p>At our coffee shop, we take pride in keeping our coffees impeccably fresh.</p>
                             <p>We source the finest beans and employ careful storage methods to preserve their aroma and taste.</p>
                             <p>With each cup, you're experiencing the vibrant and rich flavors we're dedicated to delivering.</p>
                         </div>
-                        <div className='about-2 p-5 col-3'>
+                        <div className='about-2 mt-5 mb-5 p-5 col-xxl-3 col-xl-3 col-lg-8 col-md-10 col-sm-12 col-xs-12'>
                             <h2 className='text-center'>Safe Coffee Deliveries</h2>
                             <p>Your safety is our priority, and that extends to our coffee deliveries.</p>
                             <p>We ensure all deliveries are made with the highest hygiene standards in mind.</p>
                             <p>From packaging to delivery, your coffee arrives at your doorstep with care and safety in focus.</p>
                         </div>
-                        <div className='about-3 p-5 col-3'>
+                        <div className='about-3 mt-5 mb-5 p-5 col-xxl-3 col-xl-3 col-lg-8 col-md-10 col-sm-12 col-xs-12'>
                             <h2 className='text-center'>Decades of Expertise</h2>
                             <p>With a legacy dating back to 1998, we've been committed to the art of coffee for over two decades.</p>
                             <p>Our journey started with a passion for quality and an appreciation for the perfect cup.</p>
@@ -138,34 +147,49 @@ function Main() {
                     </div>
                 </div>
 
-                <div className='four-image mt-5 mb-5 row justify-content-around align-items-center'>
+                <div className='four-image-row mt-3 mb-5 row justify-content-around align-items-center'>
+                    <div className='four-image col-xxl-12 col-xl-12 col-lg-12 col-md-12 col-sm-12 col-xs-12'>
+
+                    </div>
                 </div>
 
-                <h1 className='text-center'>PRODUCTS</h1>
-                <div className='products row justify-content-around align-items-center'>
+                <div className='products mt-5 mb-5 row justify-content-around align-items-center'>
+                    <h1 className='text-center'>PRODUCTS</h1>
                 </div>
 
                 <div className='contact-us mt-5 mb-5 row justify-content-around align-items-center'>
                     <h1 className='text-center'>CONTACT-US</h1>
-                    <div className='form col-5'>
-
+                    <div className='form mt-5 mb-5 p-5 col-xxl-5 col-xl-5 col-lg-8 col-md-10 col-sm-12 col-xs-12'>
+                        <h2 className='text-center'>Freshness at Its Best</h2>
+                        <p>At our coffee shop, we take pride in keeping our coffees impeccably fresh.</p>
+                        <p>We source the finest beans and employ careful storage methods to preserve their aroma and taste.</p>
+                        <p>With each cup, you're experiencing the vibrant and rich flavors we're dedicated to delivering.</p>
                     </div>
-                    <div className='map col-5'>
-
+                    <div className='map mt-5 mb-5 p-5 col-xxl-5 col-xl-5 col-lg-8 col-md-10 col-sm-12 col-xs-12'>
+                        <h2 className='text-center'>Freshness at Its Best</h2>
+                        <p>At our coffee shop, we take pride in keeping our coffees impeccably fresh.</p>
+                        <p>We source the finest beans and employ careful storage methods to preserve their aroma and taste.</p>
+                        <p>With each cup, you're experiencing the vibrant and rich flavors we're dedicated to delivering.</p>
                     </div>
                 </div>
 
                 <div className='accordion-row mt-5 mb-5 row justify-content-around align-items-center'>
                     <h1 className='text-center'>ACCORDION</h1>
-                    <div className='accordion col-5'>
-
+                    <div className='accordion mt-5 mb-5 p-5 col-xxl-5 col-xl-5 col-lg-8 col-md-10 col-sm-12 col-xs-12'>
+                        <h2 className='text-center'>Freshness at Its Best</h2>
+                        <p>At our coffee shop, we take pride in keeping our coffees impeccably fresh.</p>
+                        <p>We source the finest beans and employ careful storage methods to preserve their aroma and taste.</p>
+                        <p>With each cup, you're experiencing the vibrant and rich flavors we're dedicated to delivering.</p>
                     </div>
                 </div>
 
                 <div className='partner-row mt-5 mb-5 row justify-content-around align-items-center'>
                     <h1 className='text-center'>PARTNERSHIP</h1>
-                    <div className='partner col-5'>
-
+                    <div className='partner mt-5 mb-5 p-5 col-xxl-5 col-xl-5 col-lg-8 col-md-10 col-sm-12 col-xs-12'>
+                        <h2 className='text-center'>Freshness at Its Best</h2>
+                        <p>At our coffee shop, we take pride in keeping our coffees impeccably fresh.</p>
+                        <p>We source the finest beans and employ careful storage methods to preserve their aroma and taste.</p>
+                        <p>With each cup, you're experiencing the vibrant and rich flavors we're dedicated to delivering.</p>
                     </div>
                 </div>
 
--- coffee/src/components/MyFooter.js
+++ coffee/src/components/MyFooter.js
@@ -2,27 +2,27 @@ function MyFooter() {
     return (
         <>
             <footer>
-                <div className="myFooter d-flex justify-content-center">
+                <div className="myFooter d-flex justify-content-center p-5">
                     <div className="row justify-content-around align-items-center">
-                        <div className="one d-flex flex-column align-items-center justify-content-center col-2">
+                        <div className="one d-flex flex-column align-items-center justify-content-center mt-2 mb-2 col-xxl-2 col-xl-2 col-lg-12 col-md-12 col-sm-12 col-xs-12">
                             <h1>Content</h1>
                             <h1>Content</h1>
                             <h1>Content</h1>
                             <h1>Content</h1>
                         </div>
-                        <div className="two d-flex flex-column align-items-center justify-content-center col-2">
+                        <div className="two d-flex flex-column align-items-center justify-content-center mt-2 mb-2 col-xxl-2 col-xl-2 col-lg-12 col-md-12 col-sm-12 col-xs-12">
                             <h1>Content</h1>
                             <h1>Content</h1>
                             <h1>Content</h1>
                             <h1>Content</h1>
                         </div>
-                        <div className="three d-flex flex-column align-items-center justify-content-center col-2">
+                        <div className="three d-flex flex-column align-items-center justify-content-center mt-2 mb-2 col-xxl-2 col-xl-2 col-lg-12 col-md-12 col-sm-12 col-xs-12">
                             <h1>Content</h1>
                             <h1>Content</h1>
                             <h1>Content</h1>
                             <h1>Content</h1>
                         </div>
-                        <div className="four d-flex flex-column align-items-center justify-content-center col-2">
+                        <div className="four d-flex flex-column align-items-center justify-content-center mt-2 mb-2 col-xxl-2 col-xl-2 col-lg-12 col-md-12 col-sm-12 col-xs-12">
                             <h1>Content</h1>
                             <h1>Content</h1>
                             <h1>Content</h1>
