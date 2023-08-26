d9c977c85390181d7adbd92aa9e2a7a8d68c4f1b
Emircan ÇUBUKCU <emircancbkc@gmail.com>
2023-08-26 17:36:43 +0300

    settingspy-update

--- coffee_project/main_app/models.py
+++ coffee_project/main_app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(("Post Title"), max_length=50)
+    postDesc = models.TextField(("Post Description"))
+    postImg = models.ImageField((""), upload_to=None, height_field=None,width_field=None, max_length=None, null=True, blank=True)
+    postTime = models.DateTimeField((""), auto_now=False, auto_now_add=False, null=True, blank=True)
+    def __str__(self) -> str:
+        return self.title
