ed8d4cae4a9377c83db19e2c97d18eef40791630
Emircan ÇUBUKCU <emircancbkc@gmail.com>
2023-08-27 21:37:20 +0300

    product-model-add

--- coffee_project/main_app/models.py
+++ coffee_project/main_app/models.py
@@ -7,3 +7,11 @@ class Post(models.Model):
     postTime = models.DateTimeField((""), auto_now=False, auto_now_add=False, null=True, blank=True)
     def __str__(self) -> str:
         return self.title
+
+class Product(models.Model):
+    title = models.CharField(("Product Title"), max_length=50)
+    Desc = models.TextField(("Product Description"))
+    Img = models.ImageField(("Product Image"), upload_to=None, height_field=None,width_field=None, max_length=None, null=True, blank=True)
+    Price = models.PositiveIntegerField(("Price"))
+    def __str__(self) -> str:
+        return self.title
